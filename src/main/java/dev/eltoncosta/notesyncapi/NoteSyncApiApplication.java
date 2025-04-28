bd0268ab9dfa03744df725eb33c52777b3642483
Elton Alcantara <eltonalcancosta@gmail.com>
2025-04-28 12:18:41 -0300

    Configure dotenv to load environment variables for database connection

--- src/main/java/dev/eltoncosta/notesyncapi/NoteSyncApiApplication.java
+++ src/main/java/dev/eltoncosta/notesyncapi/NoteSyncApiApplication.java
@@ -8,7 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NoteSyncApiApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
+        Dotenv dotenv = Dotenv.configure()
+                .directory(".") // diretório atual
+                .ignoreIfMalformed()
+                .ignoreIfMissing()
+                .load();
+
+        System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+        System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
+        System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+
         SpringApplication.run(NoteSyncApiApplication.class, args);
     }
 

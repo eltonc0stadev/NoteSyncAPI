be07a32f66f14bc90d578dd15aa63a8076145ed2
Elton Alcantara <eltonalcancosta@gmail.com>
2025-06-02 19:48:58 -0300

    refactor: new design to project

--- src/main/java/dev/eltoncosta/notesyncapi/NoteSyncApiApplication.java
+++ src/main/java/dev/eltoncosta/notesyncapi/NoteSyncApiApplication.java
@@ -1,5 +1,6 @@
 package dev.eltoncosta.notesyncapi;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NoteSyncApiApplication {
 
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> {
+            if (System.getProperty(entry.getKey()) == null && System.getenv(entry.getKey()) == null) {
+                System.setProperty(entry.getKey(), entry.getValue());
+            }
+        });
+
         SpringApplication.run(NoteSyncApiApplication.class, args);
     }
 }
